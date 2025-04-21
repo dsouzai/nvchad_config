4807cb351de7bba6aac6033b687608e2a1d0f18b
Irwin D'Souza <dsouzai.gh@gmail.com>
2025-04-21 16:56:51 -0400

    Further improvements
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,7 +1,7 @@
 -- Need to do this before nvchad mappings
 vim.keymap.set('n', '<C-n>', '<tab>', { desc = 'Go to N newer entry in jump list' })
 
-require "nvchad.mappings"
+require 'nvchad.mappings'
 
 -- add yours here
 local map = vim.keymap.set
@@ -12,24 +12,31 @@ map('n', '<leader>fg', builtin.git_files, { desc = 'Telescope find git files' })
 map('n', '<leader>lg', builtin.live_grep, { desc = 'Telescope live grep' })
 map('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+map('n', 'gs', builtin.grep_string, { desc = 'Telescope grep string' })
+map('n', 'gd', builtin.lsp_definitions, { desc = 'Go to definition under cursor' })
+map('n', 'gr', builtin.lsp_references, { desc = 'Open a telescope window with references' })
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("n", "<leader>o", "o<Esc>", { desc = "Insert line below, stay in normal mode" })
-map("n", "<leader>O", "O<esc>", { desc = "Insert line below, stay in normal mode" })
-map("n", "<leader>bl", ":b#<Enter>", { desc = "Toggle between the most recently used buffer" })
+map('n', ';', ':', { desc = 'CMD enter command mode' })
+map('n', '<leader>c/', '/<Bslash>c', { desc = 'Case-insensitive forward search' })
+map('n', '<leader>c?', '?<Bslash>c', { desc = 'Case-insensitive forward search' })
 
-map("n", "<leader>tv", ":NvimTreeToggle<Enter>", { desc = "Toggle treeview" })
-map("n", "<leader>tf", ":NvimTreeFocus<ENter>", { desc = "Focus on treeview "})
+
+map('n', '<leader>o', 'o<Esc>', { desc = 'Insert line below, stay in normal mode' })
+map('n', '<leader>O', 'O<Esc>', { desc = 'Insert line below, stay in normal mode' })
+map('n', '<leader>bl', ':b#<CR>', { desc = 'Toggle between the most recently used buffer' })
+
+map('n', '<leader>tv', ':NvimTreeToggle<CR>', { desc = 'Toggle treeview' })
+map('n', '<leader>tf', ':NvimTreeFocus<CR>', { desc = 'Focus on treeview '})
+
+map('n', '<leader>cs', ':ClangdSwitchSourceHeader<CR>', { desc = 'Clangd switch source/header' })
 
 local lb = vim.lsp.buf
-map("n", "gd", builtin.lsp_definitions, { desc = "Go to definition under cursor" })
-map("n", "gD", function() lb.declaration() end, { desc = "Go to declaration under cursor" })
-map("n", "gt", function() lb.type_definition() end, { desc = "Go to type definition under cursor" })
-map("n", "gr", builtin.lsp_references, { desc = "Open a telescope window with references" })
+map('n', 'gD', function() lb.declaration() end, { desc = 'Go to declaration under cursor' })
+map('n', 'gt', function() lb.type_definition() end, { desc = 'Go to type definition under cursor' })
 
 -- insert mode
-map("i", "<M-b>", "<Esc>bi", { desc = "Move one word back" })
-map("i", "<M-f>", "<Esc>lwi", { desc = "Move one word forward" })
+map('i', '<M-b>', '<Esc>bi', { desc = 'Move one word back' })
+map('i', '<M-f>', '<Esc>lwi', { desc = 'Move one word forward' })
 
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- map({ 'n', 'i', 'v' }, '<C-s>', '<cmd> w <cr>')
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -129,6 +129,19 @@ return {
         "wuelnerdotexe/vim-enfocado",
     },
 
+    {
+       'rmagatti/auto-session',
+       lazy = false,
+
+       ---enables autocomplete for opts
+       ---@module "auto-session"
+       ---@type AutoSession.Config
+       opts = {
+          suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+          -- log_level = 'debug',
+       }
+    }
+
 
     -- test new blink
     -- { import = "nvchad.blink.lazyspec" },
