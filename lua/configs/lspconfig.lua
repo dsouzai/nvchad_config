d86de409a63d12f98aac1044ac884b9fcd54ac70
Irwin D'Souza <dsouzai.gh@gmail.com>
2025-04-24 02:18:57 -0400

    Add glsl to the list of local lsp servers
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "clangd", "lua_ls" }
+local servers = { "clangd", "lua_ls", "glsl_analyzer" }
 vim.lsp.config('clangd', {
     cmd = {
         'clangd',
