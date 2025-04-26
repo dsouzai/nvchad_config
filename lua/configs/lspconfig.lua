93a5c4b211bd8234734252df92c4e7b477431fab
Irwin D'Souza <dsouzai.gh@gmail.com>
2025-04-26 00:41:14 -0400

    Add rust_analyzer lsp server
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "clangd", "lua_ls", "glsl_analyzer" }
+local servers = { "clangd", "lua_ls", "glsl_analyzer", "rust_analyzer" }
 vim.lsp.config('clangd', {
     cmd = {
         'clangd',
